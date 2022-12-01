32325e707c63903d44f0a059267d81ac70c9afdd
Jamie Fairbrother <jfairbrother87@hotmail.com>
2022-12-01 12:21:58 +0000

    Moved FiniteCone tests into @testset's

--- test/test_FiniteCone.jl
+++ test/test_FiniteCone.jl
@@ -2,46 +2,47 @@
 using Test
 using Cones
 
-print("\tTest 1 - #generators == dim == 2...")
-cone_1 = FiniteCone([[0.0, 1.0] [1.0, 1.0]])
-test_data_1 = [([-1.0, 3.0], [0.0, 3.0]),
-               ([-1.0, -1.0], [0.0, 0.0]),
-               ([2.0, 0.0], [1.0, 1.0]),
-               ([0.25, 1.0], [0.25, 1.0])]
-for (x, proj_x) in test_data_1
-    @test project(cone_1, x) ≈ proj_x
+@testset "FiniteCone projection - #generators == dim == 2..." begin
+    cone_1 = FiniteCone([[0.0, 1.0] [1.0, 1.0]])
+    test_data_1 = [([-1.0, 3.0], [0.0, 3.0]),
+                ([-1.0, -1.0], [0.0, 0.0]),
+                ([2.0, 0.0], [1.0, 1.0]),
+                ([0.25, 1.0], [0.25, 1.0])]
+    for (x, proj_x) in test_data_1
+        @test project(cone_1, x) ≈ proj_x
+    end
 end
-print("success!\n")
 
-print("\tTest 2 - 3 = #generators > dim = 2...")
-cone_2 = FiniteCone([[0.0, 1.0] [1.0, 1.0] [0.5, 1.0]])
-test_data_2 = [([-1.0, 3.0], [0.0, 3.0]),
-               ([-1.0, -1.0], [0.0, 0.0]),
-               ([2.0, 0.0], [1.0, 1.0]),
-               ([0.25, 1.0], [0.25, 1.0])]
-for (x, proj_x) in test_data_2
-    @test project(cone_2, x) ≈ proj_x
+
+@testset "FiniteCone projection - 3 = #generators > dim = 2..." begin
+    cone_2 = FiniteCone([[0.0, 1.0] [1.0, 1.0] [0.5, 1.0]])
+    test_data_2 = [([-1.0, 3.0], [0.0, 3.0]),
+                ([-1.0, -1.0], [0.0, 0.0]),
+                ([2.0, 0.0], [1.0, 1.0]),
+                ([0.25, 1.0], [0.25, 1.0])]
+    for (x, proj_x) in test_data_2
+        @test project(cone_2, x) ≈ proj_x
+    end
 end
-print("success!\n")
-    
 
-print("\tTest 3 - 2 = #generators < dim = 3...")
-cone_3 = FiniteCone([[0.0, 0.0, 1.0] [1.0, 1.0, 1.0]])
-test_data_3 = [([-1.0, -1.0, 1.0], [0.0, 0.0, 1.0]),
-               ([3.0, 0.0, 0.0], [1.0, 1.0, 1.0]),
-               ([-1.0, -1.0, -1.0], [0.0, 0.0, 0.0]),
-               ([1.0, 2.0, 3.0], [1.5, 1.5, 3.0]),
-               ([2.5, 2.5, 6.0], [2.5, 2.5, 6.0])]
-for (x, proj_x) in test_data_3
-    @test project(cone_3, x) ≈ proj_x
+@testset "FiniteCone projection - 2 = #generators < dim = 3..." begin
+    cone_3 = FiniteCone([[0.0, 0.0, 1.0] [1.0, 1.0, 1.0]])
+    test_data_3 = [([-1.0, -1.0, 1.0], [0.0, 0.0, 1.0]),
+                ([3.0, 0.0, 0.0], [1.0, 1.0, 1.0]),
+                ([-1.0, -1.0, -1.0], [0.0, 0.0, 0.0]),
+                ([1.0, 2.0, 3.0], [1.5, 1.5, 3.0]),
+                ([2.5, 2.5, 6.0], [2.5, 2.5, 6.0])]
+    for (x, proj_x) in test_data_3
+        @test project(cone_3, x) ≈ proj_x
+    end
 end
-print("success!\n")
 
-print("\tTest membership # generations = 3, dim = 3")
-cone_4 = FiniteCone([[1.0, 1.0, 0.0] [1.0, 0.0, 1.0] [0.0, 1.0, 1.0]])
-test_data_4 = [([1.0, 1.0, 0.0], true),
-                ([1.0, 1.0, 1.0], true),
-                ([1.0, 0.0, 0.0], false)]
-for (x, res) in test_data_4
-    @test (x ∈ cone_4) == res
+@testset "FiniteCone membership # generations = 3, dim = 3" begin
+    cone_4 = FiniteCone([[1.0, 1.0, 0.0] [1.0, 0.0, 1.0] [0.0, 1.0, 1.0]])
+    test_data_4 = [([1.0, 1.0, 0.0], true),
+                    ([1.0, 1.0, 1.0], true),
+                    ([1.0, 0.0, 0.0], false)]
+    for (x, res) in test_data_4
+        @test (x ∈ cone_4) == res
+    end
 end
