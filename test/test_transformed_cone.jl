06fc9e7167ae539bd9ee5ed50f3be1ac8a21da03
Jamie Fairbrother <jfairbrother87@hotmail.com>
2023-01-19 12:14:20 +0000

    Minor fix to TransformedCone test

--- test/test_transformed_cone.jl
+++ test/test_transformed_cone.jl
@@ -5,8 +5,9 @@ using Cones
 @testset "TransformedCone membership" begin
     C = SOCone(2, 1.0)
     U = (I/sqrt(2)) * [[1.0, -1] [1, 1]]
-    K = TransformedCone(C, U) # SOCone rotated 90 degrees clockwise
-    @test [0.0, 1.0] ∉ K # in original but not transformed
-    @test [1.0, 0.0] ∈ K # in transformed but not original
+    K = TransformedCone(C, U) # SOCone rotated 45 degrees clockwise
+    @test [0.0, 1.0] ∉ K # not in original and not transformed
+    @test [1.0, 0.0] ∈ K # in original and transformed
+    @test [0.0, -1.0] ∈ K # not in original but in transformed
+    @test [1.0, 1.0] ∉ K # in original but not in transformed
 end
-
